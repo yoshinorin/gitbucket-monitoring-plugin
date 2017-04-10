1a2a838d0805847fed0ac303649cdeac27489ff3
YoshinoriN <nayo-msoft@outlook.jp>
2017-04-10 23:54:36 +0900

    Create get and display JVM information

--- src/main/scala/gitbucket/monitoring/controllers/MonitoringController.scala
+++ src/main/scala/gitbucket/monitoring/controllers/MonitoringController.scala
@@ -15,11 +15,20 @@ class MonitoringController extends ControllerBase with AdminAuthenticator {
 
   get("/admin/monitoring")(adminOnly {
     val osInfo = getOSInfo()
-    html.index(None, osInfo);
+    val jvmInfo = getJVMInfo()
+    html.index(None, osInfo, jvmInfo);
   })
 
   def getOSInfo(): OSInfo = {
     new OSInfo(System.getProperty("os.name"), System.getProperty("os.version"), System.getProperty("os.arch"))
+  }
+
+  def getJVMInfo(): JVMInfo = {
+    val memTotal = (Runtime.getRuntime().totalMemory() / (1024 * 1024))
+    val memFree = (Runtime.getRuntime().freeMemory() / (1024 * 1024))
+    val memUsed = memTotal - memFree
+    val memMax = (Runtime.getRuntime().maxMemory() / (1024 * 1024))
+    new JVMInfo(System.getProperty("java.vm.name"), System.getProperty("java.runtime.version"), memTotal, memFree, memUsed, memMax)
   }
 }
 
@@ -27,3 +36,12 @@ case class OSInfo (
   osName: String,
   osVersion: String,
   osArch: String)
+
+case class JVMInfo(
+  vmName: String,
+  runtimeVersion: String,
+  memTotal: Long,
+  memFree: Long,
+  memUsed: Long,
+  memMax: Long
+)
