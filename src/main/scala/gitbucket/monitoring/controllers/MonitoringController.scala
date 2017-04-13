42bc6b7de79bd05f0b1468b78cdb968dac39e5aa
YoshinoriN <nayo-msoft@outlook.jp>
2017-04-13 21:29:57 +0900

    Change directory hierarchy and rename some variables

--- src/main/scala/gitbucket/monitoring/controllers/MonitoringController.scala
+++ src/main/scala/gitbucket/monitoring/controllers/MonitoringController.scala
@@ -4,12 +4,11 @@ import io.github.gitbucket.scalatra.forms._
 import gitbucket.core.controller.ControllerBase
 import gitbucket.core.util.AdminAuthenticator
 import gitbucket.core.view.helpers._
-import gitbucket.monitoring.html
 import java.net._
 import java.io.File
 
 import scala.collection.JavaConversions._
-import gitbucket.monitoring.html._
+import gitbucket.monitoring.information.html._
 import gitbucket.monitoring.models.{MachineResources, _}
 
 class MonitoringController extends ControllerBase with AdminAuthenticator {
@@ -19,7 +18,7 @@ class MonitoringController extends ControllerBase with AdminAuthenticator {
   })
 
   get("/admin/monitoring/systeminformation")(adminOnly {
-    html.systeminformation(new SystemInformation(System.getProperty("os.name"), System.getProperty("os.version"), System.getProperty("os.arch")));
+    gitbucket.monitoring.information.html.system(new SystemInformation(System.getProperty("os.name"), System.getProperty("os.version"), System.getProperty("os.arch")));
   })
 
   get("/admin/monitoring/jvm")(adminOnly {
@@ -27,10 +26,10 @@ class MonitoringController extends ControllerBase with AdminAuthenticator {
     val memFree = (Runtime.getRuntime().freeMemory() / (1024 * 1024))
     val memUsed = memTotal - memFree
     val memMax = (Runtime.getRuntime().maxMemory() / (1024 * 1024))
-    html.jvm(new JVM(System.getProperty("java.vm.name"), System.getProperty("java.runtime.version"), memTotal, memFree, memUsed, memMax));
+    gitbucket.monitoring.information.html.jvm(new JVM(System.getProperty("java.vm.name"), System.getProperty("java.runtime.version"), memTotal, memFree, memUsed, memMax));
   })
 
   get("/admin/monitoring/machineresources")(adminOnly {
-    html.machineresources(new MachineResources(Runtime.getRuntime().availableProcessors()));
+    gitbucket.monitoring.information.html.resources(new MachineResources(Runtime.getRuntime().availableProcessors()));
   })
 }
