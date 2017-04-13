68a660e2d2e6a862cc81a504d531f8c5f61f8654
YoshinoriN <nayo-msoft@outlook.jp>
2017-04-14 00:49:28 +0900

    Add get disk usaga method and display it on resources view

--- src/main/scala/gitbucket/monitoring/models/MachineResources.scala
+++ src/main/scala/gitbucket/monitoring/models/MachineResources.scala
@@ -1,5 +1,12 @@
 package gitbucket.monitoring.models
 
+import java.nio.file._
+import gitbucket.monitoring.utils.{ByteConverter}
+
 class MachineResources extends OperatingSystem() {
+  def fileStore = Files.getFileStore(Paths.get("."))
   def core = Runtime.getRuntime().availableProcessors()
+  def totalSpace = ByteConverter.ByteToGB(fileStore.getTotalSpace())
+  def freeSpace = ByteConverter.ByteToGB(fileStore.getUnallocatedSpace())
+  def usedSpace = totalSpace - freeSpace
 }
