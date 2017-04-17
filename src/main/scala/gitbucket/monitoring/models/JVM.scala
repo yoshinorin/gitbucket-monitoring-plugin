4eab22c19f7bfd10111ad0288e99fa842ac4270d
YoshinoriN <nayo-msoft@outlook.jp>
2017-04-17 23:04:44 +0900

    Change Java runtime version to Java version

--- src/main/scala/gitbucket/monitoring/models/JVM.scala
+++ src/main/scala/gitbucket/monitoring/models/JVM.scala
@@ -4,7 +4,7 @@ import gitbucket.monitoring.utils.{ByteConverter}
 
 class JVM () {
   val vmName = System.getProperty("java.vm.name")
-  val runtimeVersion = System.getProperty("java.runtime.version")
+  val javaVersion = System.getProperty("java.version")
   val memTotal = (ByteConverter.ByteToMB(Runtime.getRuntime().totalMemory()))
   val memFree = (ByteConverter.ByteToMB(Runtime.getRuntime().freeMemory()))
   val memUsed = memTotal - memFree
