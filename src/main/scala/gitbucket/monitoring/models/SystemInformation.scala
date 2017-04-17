e5f3dd0afeb33b2d92de3a630ea9114037db1fc3
YoshinoriN <nayo-msoft@outlook.jp>
2017-04-17 21:36:14 +0900

    Add system time information

--- src/main/scala/gitbucket/monitoring/models/SystemInformation.scala
+++ src/main/scala/gitbucket/monitoring/models/SystemInformation.scala
@@ -1,5 +1,11 @@
 package gitbucket.monitoring.models
 
-class SystemInformation extends OperatingSystem () {
+import java.util._
+import java.time._
 
+class SystemInformation extends OperatingSystem () {
+  def nowTime = LocalDateTime.now()
+  def timeZone = ZoneId.systemDefault()
+  def zoneOffset = timeZone.getRules().getOffset(nowTime)
+  def dayOfWeek = nowTime.getDayOfWeek()
 }
