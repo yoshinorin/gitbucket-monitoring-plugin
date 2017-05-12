c3d536f41060bab008a1f0772cefb24f5b17d441
YoshinoriN <nayo-msoft@outlook.jp>
2017-05-13 03:01:32 +0900

    Create convert seconds to DateTime method

--- src/main/scala/gitbucket/monitoring/utils/Calc.scala
+++ src/main/scala/gitbucket/monitoring/utils/Calc.scala
@@ -1,5 +1,7 @@
 package gitbucket.monitoring.utils
 
+import java.util.concurrent.TimeUnit
+
 object UnitConverter {
   def byteToKB(byte: BigDecimal, digit: Int = 2): BigDecimal = Rounding.ceil(byte / 1024)
   def byteToMB(byte: BigDecimal, digit: Int = 2): BigDecimal = Rounding.ceil(byte / (1024 * 1024))
@@ -9,5 +11,25 @@ object UnitConverter {
 object Rounding {
   def ceil(num:BigDecimal, digit: Int = 2): BigDecimal = {
     num.setScale(digit, scala.math.BigDecimal.RoundingMode.CEILING)
+  }
+}
+
+object Time {
+  case class DateTime(
+    days: Int,
+    hours: Int,
+    minutes: Int
+  )
+
+  def secondsToDateTime(seconds: Int): Either[String, DateTime] = {
+    try {
+      Right(DateTime(
+        (seconds / (60 * 60 * 24)) % 60,
+        ((seconds / (60 * 60)) % 24) % 60,
+        (seconds / 60) % 60
+      ))
+    } catch {
+      case e: Exception => Left("ERROR")
+    }
   }
 }
