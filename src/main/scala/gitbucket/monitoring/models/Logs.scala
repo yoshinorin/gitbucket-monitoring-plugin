7e295f48253b624598cd069432780a82e197a237
YoshinoriN <nayo-msoft@outlook.jp>
2017-05-14 00:06:19 +0900

    Refactoring get logback configuration file

--- src/main/scala/gitbucket/monitoring/models/Logs.scala
+++ src/main/scala/gitbucket/monitoring/models/Logs.scala
@@ -1,25 +1,20 @@
 package gitbucket.monitoring.models
 
+import java.nio.file.{Files, Paths}
+import gitbucket.core.util.StringUtil
 import gitbucket.monitoring.utils._
 
 object LogBack {
 
   val enableLogging = Java.all.contains("logback.configurationFile")
   val confPath = Java.all.getOrElse("logback.configurationFile", "Can not find logback-settings.xml")
-
-  def xmlContent: Either[String, String] = {
+  val xmlContent: Either[String, String] = {
     if (enableLogging) {
       try {
-        val s = scala.io.Source.fromFile(confPath)
-        try {
-          (Right(
-            s.mkString
-          ))
-        } catch {
-          case e: Exception => Left("ERROR")
-        } finally {
-          s.close
-        }
+        val bytes = Files.readAllBytes(Paths.get(confPath))
+        (Right(
+          StringUtil.convertFromByteArray(bytes)
+        ))
       } catch {
         case e: Exception => Left("ERROR")
       }
