3e76f8644cb21d398f85d4d0ffcfb22310321b51
YoshinoriN <nayo-msoft@outlook.jp>
2017-11-30 21:32:32 +0900

    Use JavaConverters

--- src/main/scala/gitbucket/monitoring/services/EnvironmentVariable.scala
+++ src/main/scala/gitbucket/monitoring/services/EnvironmentVariable.scala
@@ -1,10 +1,10 @@
 package gitbucket.monitoring.services
 
-import scala.collection.JavaConversions._
+import scala.collection.JavaConverters._
 import gitbucket.monitoring.utils.Message
 
 object EnvironmentVariable {
   def valiables: Map[String, String] = {
-    System.getenv().toMap
+    System.getenv().asScala.toMap
   }
 }
