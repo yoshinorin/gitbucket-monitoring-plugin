d591864cc5b571571e054524fce4f3e3e96160f8
YoshinoriN <nayo-msoft@outlook.jp>
2017-11-30 21:30:26 +0900

    Refactoring delete no need try-catch

--- src/main/scala/gitbucket/monitoring/services/EnvironmentVariable.scala
+++ src/main/scala/gitbucket/monitoring/services/EnvironmentVariable.scala
@@ -4,11 +4,7 @@ import scala.collection.JavaConversions._
 import gitbucket.monitoring.utils.Message
 
 object EnvironmentVariable {
-  def valiables: Either[String, Map[String, String]] = {
-    try {
-      Right(System.getenv().toMap)
-    } catch {
-      case e: Exception => Left(Message.error)
-    }
+  def valiables: Map[String, String] = {
+    System.getenv().toMap
   }
 }
