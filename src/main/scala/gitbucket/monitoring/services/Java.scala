8731c5005be6878a4a0df78bca01f0394919b980
YoshinoriN <nayo-msoft@outlook.jp>
2017-12-01 21:34:37 +0900

    Use JavaConverters

--- src/main/scala/gitbucket/monitoring/services/Java.scala
+++ src/main/scala/gitbucket/monitoring/services/Java.scala
@@ -1,13 +1,13 @@
 package gitbucket.monitoring.services
 
-import scala.collection.JavaConversions._
+import scala.collection.JavaConverters._
 import gitbucket.monitoring.utils._
 
 object Java {
   val memTotal = (UnitConverter.byteToMB(Runtime.getRuntime().totalMemory()))
   val memMax = (UnitConverter.byteToMB(Runtime.getRuntime().maxMemory()))
 
-  def getSystemProperties: Map[String, String] = System.getProperties().toMap
+  def getSystemProperties: Map[String, String] = System.getProperties().asScala.toMap
 
   def getMemoryInfo: Memory = {
     val memFree = UnitConverter.byteToMB(Runtime.getRuntime().freeMemory())
