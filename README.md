8f8be27297d7f5588087383812cf2f5147107342
YoshinoriN <nayo-msoft@outlook.jp>
2018-01-22 22:00:18 +0900

    v1.2.0

--- README.md
+++ README.md
@@ -40,6 +40,7 @@ Goto the `System Administration` menu, you can see `Monitoring` section.
 
 |Plugin version|GitBucket version|
 |:-------------:|:-------:|
+|1.2.0|4.10 - 4.20.0|
 |1.1.0|4.10 - 4.20.0|
 |1.0.0|4.10 - 4.20.0|
 
--- src/main/scala/Plugin.scala
+++ src/main/scala/Plugin.scala
@@ -10,7 +10,8 @@ class Plugin extends gitbucket.core.plugin.Plugin {
   override val description: String = "Display machine information, resources, processes, Java information and GitBucket's log."
   override val versions: List[Version] = List(
     new Version("1.0.0"),
-    new Version("1.1.0")
+    new Version("1.1.0"),
+    new Version("1.2.0")
   )
 
   override val systemSettingMenus: Seq[(Context) => Option[Link]] = Seq(
